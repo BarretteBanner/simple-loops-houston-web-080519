aaa1d6e3c55920424c084bd4c90e6c2f5dbbb6c3
Barrette Banner <barrettebanner@gmail.com>
2019-08-02 18:32:18 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,8 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   index = 0 
-  while index < number_of_times
+  loop do
+   index < number_of_times
     return phrase
     index += 1 
   end
