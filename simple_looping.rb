a29a2e69c88f84e74f053ff4c3a6be7df585af43
Barrette Banner <barrettebanner@gmail.com>
2019-08-02 18:32:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   index = 0 
   loop do
-   index < number_of_times
+   index += 1 
     return phrase
     index += 1 
   end
